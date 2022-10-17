91fad006b95242d8c0e3064152e8c4637a95fd42
Brad Ensign <51426961+EnsignCreative@users.noreply.github.com>
2022-10-17 16:42:14 -0400

    Initial Commit

---
+++ NewsLeak/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NewsLeak
+//
+//  Created by Bradley Ensign on 10/17/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
